245e0672360b4a1d821356a26fb8e402c0b6a34e
Dewy <dewy@dewy.dev>
2020-08-02 19:06:06 +0100

    bump 3.1.0

--- src/main/java/dev/dewy/dqs/utils/Constants.java
+++ src/main/java/dev/dewy/dqs/utils/Constants.java
@@ -53,9 +53,8 @@ import java.util.Date;
 
 public final class Constants
 {
-    public static final String VERSION = "3.0.3";
+    public static final String VERSION = "3.1.0";
 
-    public static final JsonParser JSON_PARSER = new JsonParser();
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     public static final DefaultLogger DEFAULT_LOG = Logging.logger;
